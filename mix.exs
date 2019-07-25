275fe623676566188d7b3cb4898d480f3f965a17
Frank Vumbaca <fvumbaca@outlook.com>
2019-07-25 08:11:20 -0400

    Added hex metadata to prep for publishing

--- mix.exs
+++ mix.exs
@@ -4,10 +4,17 @@ defmodule PrometheusDef.MixProject do
   def project do
     [
       app: :prometheus_def,
-      version: "0.0.0",
+      version: "0.0.0-alpha.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/azohra/prometheus_def",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
     ]
   end
 
@@ -17,8 +24,22 @@ defmodule PrometheusDef.MixProject do
     ]
   end
 
+  defp description() do
+    "Quality of life improvements when working with Prometheus"
+  end
+
+  defp package() do
+    [
+      licences: ["MIT"],
+      links: %{
+        "Azohra" => "https://github.com/azohra"
+      }
+    ]
+  end
+
   defp deps do
     [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:prometheus_ex, "~> 3.0", optional: true},
     ]
   end
